73cd2f93fe244e29356fdc433ac3a687ffaab27f
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-01-25 14:55:18 +0900

    user name, greetings
    Completed with getting and saving user name.

--- clock.js
+++ clock.js
@@ -1,16 +1,19 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector(".js-clock__title");
 
-    //".js-clock__title"
+    console.log(clockTitle);
+
 function getTime() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    clockTitle.innerText = `${hours}:${minutes}`;
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}`:hours}:${minutes < 10 ? `0${minutes}`:minutes}:${seconds < 10 ? `0${seconds}`:seconds}`;
 }
 
 function init() {
     getTime();
+    setInterval(getTime, 1000);
 }
 
 init();
---
+++ greetings.js
@@ -0,0 +1,49 @@
+const form = document.querySelector(".js-name-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOW_CN = "show-form";
+    
+
+function saveUserName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    saveUserName(currentValue);
+    paintGreetings(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOW_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreetings(text){ // What is this 'text' thing? Is it a pre-defined constant?
+    form.classList.remove(SHOW_CN);
+    greeting.classList.add(SHOW_CN);
+    greeting.innerText = `Hi, ${text}!`;
+}
+
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        // when he has not said his name
+        askForName();
+    }
+    else{
+        paintGreetings(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+
+
+init();
